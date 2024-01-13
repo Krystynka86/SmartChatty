3571732f4b02d236cee16b7d3ce32a8b772539da
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2024-01-13 20:55:43 +0100

    Initial Commit

---
+++ SmartChatty/SmartChattyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartChattyApp.swift
+//  SmartChatty
+//
+//  Created by Cristina on 2024-01-13.
+//
+
+import SwiftUI
+
+@main
+struct SmartChattyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
