f954a9143fa511ab135ec8773f4f329c90779655
Krystynka86 <71220344+Krystynka86@users.noreply.github.com>
2024-01-21 22:30:30 +0100

    Ignore Configutarion.xcconfig file

---
+++ SmartChatty/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SmartChatty
+//
+//  Created by Cristina on 2024-01-18.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
